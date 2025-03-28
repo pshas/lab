d9fad1484ee41a875cf79e2bf5232af7ff8490f9
Ptitsyn Pavel <PtitsynPA@gaz.ru>
2025-03-28 12:09:23 +0300

    fix

--- application/index.php
+++ application/index.php
@@ -167,7 +167,7 @@ VALUES (:block_id, :USER_ID, :NAME_IZD, :ID_I, :COUNTRY, :work_department, :full
 			&nbsp;
 		</td>
 		<td colspan="4">
-			<div style="width: 40%;"> 
+			<div style="width: 40%; display: flex;"> 
 				<input type="checkbox" id="material1" name="material1" value="Plastic">
   				<label for="material1"> Plastic</label><br>
   				<input type="checkbox" id="material2" name="material2" value="Metal">
@@ -175,7 +175,7 @@ VALUES (:block_id, :USER_ID, :NAME_IZD, :ID_I, :COUNTRY, :work_department, :full
   				<input type="checkbox" id="material" name="material3" value="Painted">
   				<label for="material3"> Painted</label>
 			</div>
-			<div style="width: 60%;"> 
+			<div style="width: 60%; display: flex;"> 
 				<input type="checkbox" id="processing1" name="processing1" value="Zeiss">
   				<label for="processing1"> Zeiss</label><br>
   				<input type="checkbox" id="processing2" name="processing2" value="LaserTracer">
