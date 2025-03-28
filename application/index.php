1794e5fd5a119d157c6c9a45b2569312ad16c9ef
Ptitsyn Pavel <PtitsynPA@gaz.ru>
2025-03-28 10:50:46 +0300

    fix

--- application/index.php
+++ application/index.php
@@ -295,10 +295,10 @@ VALUES (:block_id, :USER_ID, :NAME_IZD, :ID_I, :COUNTRY, :work_department, :full
 		</td>
 	</tr>
 	<tr>
-		<td class="td-col" colspan="1" style="width: 50%;">
+		<td class="td-col" colspan="1">
 			&nbsp;
 		</td>
-		<td class="td-col" rowspan="1" colspan="1" style="width: 50%;">
+		<td class="td-col" rowspan="1" colspan="1">
 			&nbsp;
 		</td>
 	</tr>
