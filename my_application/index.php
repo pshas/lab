062935cf7061f20ad6084f5eeed3dae422d124b2
Ptitsyn Pavel <PtitsynPA@gaz.ru>
2025-04-24 10:37:51 +0300

    fix

--- my_application/index.php
+++ my_application/index.php
@@ -2,6 +2,9 @@
 	require("../header.php");
 ?>
 
+<!-- Bootstrap CSS -->
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
 <!-- Bootstrap JS (зависит от jQuery) -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 
