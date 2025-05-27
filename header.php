54ec284e55fab4739dac786d01f7ded07fab948d
Ptitsyn Pavel <PtitsynPA@gaz.ru>
2025-05-27 11:02:01 +0300

    fix

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
-<? /* require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php"); 
-$APPLICATION->SetTitle("Lab"); */
+<?  require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php"); 
+$APPLICATION->SetTitle("Lab"); 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php"); 
 ?>
 
